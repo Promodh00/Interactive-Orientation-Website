f56d067c8b03c884979e39fe764894d1a31c21c5
sakith11 <57568111+sakith11@users.noreply.github.com>
2025-08-27 21:50:25 +0530

    sakith-add building page and cosmetic changes

--- main-website/src/pages/Home.jsx
+++ main-website/src/pages/Home.jsx
@@ -14,8 +14,10 @@ export default function Home() {
                 <h1 className="welcome1">Technology</h1>
                 <h1 className="welcome3">Discover where learning meets fun, </h1>
                 <h1 className="welcome3">friends, and endless possibilities. </h1>
-                <Link to="/map">
-                    <button className="welcome-button"> Start Exploring <i className="fas fa-arrow-right"></i></button>
+                <Link to="/map" className="welcome-link">
+                    <button className="welcome-button">
+                        Start Exploring <i className="fas fa-arrow-right"></i>
+                    </button>
                 </Link>
 
             </div>
@@ -25,7 +27,9 @@ export default function Home() {
                         <tr>
                             <td colSpan="2" className="merged-cell top">
                                 <div className="cell-content">
+
                                     <h2>Common Guidlines</h2>
+                                    <img src="./images/commonGuidlines.png" className="cell-bg-image" />
 
                                     <button className="cell-button">
                                         <i className="fas fa-arrow-right"></i>
@@ -38,11 +42,14 @@ export default function Home() {
                             <td className="merged-cell bottom">
                                 <div className="cell-content">
                                     <h2>Meet the team</h2>
+                                    <img src="./images/meetTheTeam.png" className="cell-bg-image" />
+
                                     <button className="cell-button"><i className="fas fa-arrow-right"></i></button>
                                 </div>
                             </td>
                             <td className="merged-cell bottom">
                                 <div className="cell-content">
+                                    <img src="./images/roles.png" className="cell-bg-image" />
                                     <h2>Roles</h2>
                                     <button className="cell-button"><i className="fas fa-arrow-right"></i></button>
                                 </div>
